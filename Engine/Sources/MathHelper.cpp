76cf6b9aba778a3e70b52a19af2d0d1949a3da50
CynthiaBon <cynthia.bon@e-artsup.net>
2024-10-03 18:21:28 +0200

    Initial commit

---
+++ Engine/Sources/MathHelper.cpp
@@ -0,0 +1,36 @@
+#include "../Includes/MathHelper.h"
+
+float lerp(float a, float b, float t)
+{
+	return a * (1.0 - t) + (b * t);
+}
+
+glm::vec2 lerp(glm::vec2 a, glm::vec2 b, float t)
+{
+	return glm::vec2(lerp(a.x, b.x, t), lerp(a.y, b.y, t));
+}
+
+glm::vec3 lerp(glm::vec3 a, glm::vec3 b, float t)
+{
+	return glm::vec3(lerp(a.x, b.x, t), lerp(a.y, b.y, t), lerp(a.z, b.z, t));
+}
+
+glm::vec4 lerp(glm::vec4 a, glm::vec4 b, float t)
+{
+	return glm::vec4(lerp(a.x, b.x, t), lerp(a.y, b.y, t), lerp(a.z, b.z, t), lerp(a.w, b.w, t));
+}
+
+float easeInQuad(float x)
+{
+	return x * x;
+}
+
+float easeOutQuad(float x)
+{
+	return 1 - (1 - x) * (1 - x);
+}
+
+float easeInOutQuad(float x)
+{
+	return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2;
+}
---
+++ Sources/main.cpp
@@ -0,0 +1,71 @@
+#include "../Engine/Includes/GameEngine.h"
+#include "../Engine/Includes/FlyCam.h"
+
+/*
+	Keywords :
+	- include
+	- function / method
+	- public / private
+	- guard
+	- throw / exception
+	- try / catch
+	- pointer
+	- SOLID principles
+*/
+
+void initializeEngine()
+{
+	const char* window_name = "Open GL Window";
+	int window_width = 640;
+	int window_height = 480;
+
+	ShaderData shader_list[] = {
+		"Base Shader", "Engine/GLSL/vertex_core.glsl", "Engine/GLSL/fragment_core.glsl", nullptr, false,
+		"UI Shader", "Engine/GLSL/vertex_ui.glsl", "Engine/GLSL/fragment_ui.glsl", nullptr, true,
+		"Text Shader", "Engine/GLSL/vertex_ui.glsl", "Engine/GLSL/fragment_text.glsl", nullptr, true
+	};
+	int shader_array_size = sizeof(shader_list) / sizeof(ShaderData);
+
+	GameEngine::initializeEngine(window_name, window_width, window_height, shader_list, shader_array_size);
+}
+
+void runGame()
+{
+	GameObject* camera = Camera::getMain()->getGameObject();
+	FlyCam* camera_controller = new FlyCam(camera, 5.0f, 10.0f);
+
+	while (!GameEngine::getInstance()->shouldClose())
+	{
+		try
+		{
+			GameEngine::getInstance()->update();
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "ERROR: " << e.what() << "\n";
+		}
+	}
+
+	GameEngine::getInstance()->exit();
+}
+
+void GLAPIENTRY debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei lenght, const GLchar* message, const void* user_param)
+{
+	std::cout << message << "\n";
+}
+
+void enableDebugMode()
+{
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(debugCallback, 0);
+}
+
+int main()
+{
+	initializeEngine();
+
+	runGame();
+
+	return 0;
+}
